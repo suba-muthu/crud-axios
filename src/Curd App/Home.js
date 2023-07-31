8d9d9751a4e7a406384e8f3faeabd2d85090bc1b
suba <subalakshmimuthu02@gmail.com>
2023-07-31 15:56:03 +0530

    show the data in table

--- src/Curd App/Home.js
+++ src/Curd App/Home.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3030/users")
+    .then(res => setData(res.data))
+    .catch(err => console.log(err))
+  }, []);
+
   return (
-    <div></div>
+    <div classsName='container mt-5'>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item, idx) => (
+              <tr key={idx}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
