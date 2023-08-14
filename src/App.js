c9c71e44fb2b41fa1dba75e592dab2593b05572d
suba <subalakshmimuthu02@gmail.com>
2023-08-14 15:18:01 +0530

    Add data in table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Curd App/Home';
+import Create from './Curd App/Create';
 // import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />}></Route>
+        <Route path='/create' element={<Create />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/Curd App/Home.js
+++ src/Curd App/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   const [data, setData] = useState([]);
@@ -12,12 +13,15 @@ export default function Home() {
 
   return (
     <div classsName='container mt-5'>
+      <h2>Crud App with Json Server</h2>
+      <Link to="/create" className='btn btn-success my-3'>Create +</Link>
       <table className='table'>
         <thead>
           <tr>
             <th>ID</th>
             <th>Name</th>
             <th>Email</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
@@ -27,6 +31,11 @@ export default function Home() {
                 <td>{item.id}</td>
                 <td>{item.name}</td>
                 <td>{item.email}</td>
+                <td>
+                  <button>Edit</button>
+                  <button>Delete</button>
+                  <button>Read</button>
+                </td>
               </tr>
             ))
           }
